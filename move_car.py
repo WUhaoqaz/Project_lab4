301d5df3f34a13a35209fb49abdedb6bf32d543c
WUhaoqaz <2529607397@qq.com>
2025-10-09 12:37:51 +0800

    Update move_car.py

--- move_car.py
+++ move_car.py
@@ -1,1 +1,33 @@
+ import tinybit
+from microbit import display, Image, sleep
+
+def basic_movement_demo():
+    display.show(Image.HAPPY)
+    sleep(1000)
+
+    display.show(Image.ARROW_S)
+    tinybit.car_run(150)
+    sleep(2000)
+    tinybit.car_stop()
+
+    display.show(Image.ARROW_N)
+    tinybit.car_back(150)
+    sleep(2000)
+    tinybit.car_stop()
+
+    display.show(Image.ARROW_E)
+    tinybit.car_left(150)
+    sleep(1000)
+    tinybit.car_stop()
+
+    display.show(Image.ARROW_W)
+    tinybit.car_right(150)
+    sleep(1000)
+    tinybit.car_stop()
+
+    display.show(Image.YES)
+    sleep(1000)
+    display.clear()
+
+basic_movement_demo()
 
